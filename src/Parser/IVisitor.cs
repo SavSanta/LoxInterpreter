9f14ca6197f31a3912f16406ef12a066a6006b72
administrator <sav@home.win>
2024-12-22 11:04:33 -0500

    Visitor interfaceI impl as ExprVisitor

---
+++ src/Parser/IVisitor.cs
@@ -0,0 +1,13 @@
+﻿namespace LoxInterpreter
+{
+    namespace Parser
+    {
+        public interface IExprVisitor
+        {
+            IExprVisitor visitBinaryExprBase(Binary exprbase);
+            IExprVisitor visitGroupingExprBase(Grouping exprbase);
+            IExprVisitor visitLiteralExprBase(Literal exprbase);
+            IExprVisitor visitUnaryExprBase(Unary exprbase);
+        }
+    }
+}
