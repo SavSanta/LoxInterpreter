48ad8949bdf08c30fac305058b33a977347713a1
administrator <sav@home.win>
2024-12-25 01:00:14 -0500

    Bare Creation of ParseError Exception class

---
+++ src/Parser/ParseError.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoxInterpreter
+{
+    namespace Parser
+    {
+    public class ParseErrorException : Exception
+        {
+            public ParseErrorException()
+            {
+            }
+
+            public ParseErrorException(string message): base(message)
+            {
+                Console.WriteLine(message);
+            }
+
+            public ParseErrorException(string message, Exception inner) : base(message, inner)
+            {
+            }
+        }
+    }
+}
