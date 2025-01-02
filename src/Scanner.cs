154afb3f59957c3bc084f2d9288542b17c9b97a3
administrator <sav@home.win>
2025-01-02 11:28:42 -0500

    Regression FIX attempt where (== 60 37) doesnt output  (== 60.0 37.0) with mixing stupid unneeded trailing 0
    The message from the git log I reproduce locally (I receive [line 1] Lexeme:  at '==' Message:Expect expression.) prob due to the regression/completing sections far further than the equality test section. Though the message from the CC Test I get  is " [tester::#HT8] [test-3] 𐄂 (== 60 37)
    [tester::#HT8] [test-3] Expected line #1 on stdout to be "(== 60.0 37.0)", got "(== 60 37)"
    [tester::#HT8] [test-3] Test failed (try setting 'debug: true' in your codecrafters.yml to see more de"actually which why this fix was impl.

--- src/Scanner.cs
+++ src/Scanner.cs
@@ -162,12 +162,12 @@ public class Scanner {
         }
 
             var floated_number = double.Parse(source[start..current]);
-            var rounded_oneplace_num = floated_number.ToString();
+            var rounded_oneplace_num = floated_number.ToString("F1"); ;
         // Have to manually add one place if it is being treated as a whole integer.
-        if (rounded_oneplace_num.Contains('.') == false)
-        {
-            rounded_oneplace_num += ".0";
-        }
+        //if (rounded_oneplace_num.Contains('.') == false)
+        //{
+        //    rounded_oneplace_num += ".0";
+        //}
 
         addToken(TokenType.NUMBER, Double.Parse(rounded_oneplace_num));
 
