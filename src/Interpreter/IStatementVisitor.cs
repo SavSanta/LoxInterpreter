e64070eefffab822003bebe5267e10963acb97ac
administrator <sav@home.win>
2025-01-03 17:33:30 -0500

    Create new Statement Visitor Interfaces stolen from ChrisViral fully complete implementation
    Most of these  will need to be delted to align with the steps we're on in Chapter 8 but for now

---
+++ src/Interpreter/IStatementVisitor.cs
@@ -0,0 +1,33 @@
+﻿namespace LoxInterpreter.Interpreter
+{
+    public interface IStatementVisitor
+    {
+        void visitExpressionStatement(ExpressionStatement statement);
+        void visitPrintStatement(PrintStatement statement);
+        void visitReturnStatement(ReturnStatement statement);
+        void visitIfStatement(IfStatement statement);
+        void visitWhileStatement(WhileStatement statement);
+        void visitForStatement(ForStatement statement);
+        void visitBlockStatement(BlockStatement block);
+        void visitVariableDeclaration(VariableDeclaration declaration);
+        void visitFunctionDeclaration(FunctionDeclaration declaration);
+        void visitMethodDeclaration(MethodDeclaration declaration);
+        void visitClassDeclaration(ClassDeclaration declaration);
+    }
+
+    // Genericized Version
+    public interface IStatementVisitor<out T>
+    {
+        T visitExpressionStatement(ExpressionStatement statement);
+        T visitPrintStatement(PrintStatement statement);
+        T visitReturnStatement(ReturnStatement statement);
+        T visitIfStatement(IfStatement statement);
+        T visitWhileStatement(WhileStatement statement);
+        T visitForStatement(ForStatement statement);
+        T visitBlockStatement(BlockStatement block);
+        T visitVariableDeclaration(VariableDeclaration declaration);
+        T visitFunctionDeclaration(FunctionDeclaration declaration);
+        T visitMethodDeclaration(MethodDeclaration declaration);
+        T visitClassDeclaration(ClassDeclaration declaration);
+    }
+}
